87751a4b84c107c8575e43f95cce1b80e675cd3c
Emily <emily.silkin@gmail.com>
2025-04-24 19:18:49 -0400

    Added function

--- CodePath Prework/CodePath Prework/ViewController.swift
+++ CodePath Prework/CodePath Prework/ViewController.swift
@@ -11,10 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additi#imageLiteral(resourceName: "simulator_screenshot_DD69DC20-6318-4AC0-94C1-4E384047B87D.png")onal setup after loading the view.
         
     }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
 
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
 }
 
