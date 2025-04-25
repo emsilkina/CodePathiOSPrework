75e288ce8b55c811cd484bdb787bfaa5b0ba0f31
Emily <emily.silkin@gmail.com>
2025-04-24 20:04:23 -0400

    Edit University Name

--- CodePath Prework/CodePath Prework/ViewController.swift
+++ CodePath Prework/CodePath Prework/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         // Do any additi#imageLiteral(resourceName: "simulator_screenshot_DD69DC20-6318-4AC0-94C1-4E384047B87D.png")onal setup after loading the view.
         
     }
-
+    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func setUniversityName(_ sender: Any) {
+        let name=textField.text
+        textView.text=name
+    }
+    
+    
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
         func changeColor() -> UIColor{
 
